5432c833bb348704114fe01fe34511d42d6a34cc
korob <korob@192.168.1.180>
2016-04-06 09:43:52 +0500

    исправлен способ получения списка характеристик в настройах

--- lib/actions/shopLogsisPluginSettings.action.php
+++ lib/actions/shopLogsisPluginSettings.action.php
@@ -6,7 +6,7 @@ class shopLogsisPluginSettingsAction extends waViewAction {
         $app_settings_model = new waAppSettingsModel();
         $settings = $app_settings_model->get(shopLogsisPlugin::$plugin_id);
         $feature_model = new shopFeatureModel();
-        $features = $feature_model->getFeatures(true, null, 'id');
+        $features = $feature_model->getByField('type', array('dimension.weight', '3d.dimension.length'), true);
 
         $this->view->assign('settings', $settings);
         $this->view->assign('features', $features);
